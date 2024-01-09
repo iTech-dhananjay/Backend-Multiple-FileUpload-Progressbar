782223ff53a2c95a4e83bcbfaab651d40eb29418
Dhananjay Kumar <>
2024-01-09 14:29:27 +0530

    Add Multiple file uploads with progress bar status backend

---
+++ README.md
@@ -0,0 +1,2 @@
+# upload_img
+# chunkFile
---
+++ upload.js
@@ -0,0 +1,13 @@
+import multer from "multer"
+
+//  console.log("upload") 
+const  storage = multer.diskStorage({
+    destination: "uploads",
+    filename: (req, file, cb) => {
+      return  cb(null, file.fieldname + '-' + Date.now()+file.originalname)
+    }
+});
+  
+const upload = multer({ storage: storage });
+
+export default upload
